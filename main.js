486a622f859ba2568966177edd9d5195e0c0176c
a23178259 <a1993820829@gmail.com>
2025-09-28 00:40:56 +0800

    使用第三方登入驗證系統：Firebase

--- main.js
+++ main.js
@@ -1,2 +1,6 @@
 import "./assets/scss/all.scss";
 import "bootstrap/dist/js/bootstrap.min.js";
+import "./firebase.js";
+import "./auth.js";
+import "./layout/volunteerStories.js";
+import "./layout/login.js";
