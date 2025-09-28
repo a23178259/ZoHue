d3bf72bce1f954d8284627aedf97832fa561e6a0
a23178259 <a1993820829@gmail.com>
2025-09-28 20:27:33 +0800

    重新設定 Bootstrap 引入

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 import "./assets/scss/all.scss";
-import "bootstrap/dist/js/bootstrap.min.js";
+import * as bootstrap from "bootstrap";
 import "./firebase.js";
 import "./auth.js";
 import "./layout/volunteerStories.js";
 import "./layout/login.js";
+import "./layout/header.js";
+
+// 使bootstrap 物件(CSS、JS)可在全域範圍使用
+window.bootstrap = bootstrap;
