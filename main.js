27f4afebd3b91d32ead64e1fd4be3130afe30070
a23178259 <a1993820829@gmail.com>
2025-10-30 23:00:11 +0800

    把js從FAQ.html拉出來

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ import "./assets/JS/header.js";
 import "./assets/JS/footer.js";
 import "./assets/JS/project-cards-fab.js";
 import "./assets/JS/about.js";
+import "./assets/JS/FAQ.js";
 
 // 使bootstrap 物件(CSS、JS)可在全域範圍使用
 window.bootstrap = bootstrap;
